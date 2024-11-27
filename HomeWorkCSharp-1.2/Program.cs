fe4bd8223bf94098947537cbba55a62109e8e4f2
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 11:06:40 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.2. Вывести на экран число e (основание натурального логарифма) с точностью до десятых.
+            double numberE = Math.E; //Определяем переменную и присваеваем ей константу (E).
+            double result = Math.Round(numberE, 1); //сокращаем до 1го символова после целого числа.
+            Console.WriteLine(result);
+        }
+    }
+}
